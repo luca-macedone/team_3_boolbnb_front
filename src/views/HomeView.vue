e8c67e6d7594e2ff2195121be7b8e2215f23adbc
RobertoNesta <robertonesta1993@gmail.com>
2023-07-06 17:54:50 +0200

    delete internal folder and move all pictures and logos in the public folder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,9 +16,9 @@ export default {
     <!-- search ------------------>        
         <div class="d-flex justify-content-center">
             <div class="d-flex align-items-center rounded-3 shadow gap-3 search_box ">
-                <img class="pe-3" height="30" src="../../public/internal/pin_only.svg" alt="">
-                <input class=" search_input rounded-3 shadow " type="search" name="search" id="search"
-                    placeholder="  Where we go?">
+                <img class="pe-3" height="30" src="../../public/pin_only.svg" alt="">
+                <input class="search_input rounded-3 shadow" type="search" name="search" id="search"
+                    placeholder="Where are you going now?">
                     <div class="col d-flex justify-content-between py-3">
                         <a type="button" class="btn back_btn d-flex align-items-center gap-2 shadow"
                             href="#">
@@ -41,7 +41,7 @@ export default {
 
             <div class="col-12 col-lg-6">
                 <div class="card m-auto p-3 m-0 w-100">
-                    <img src="../../public/internal/pin_only.svg" class="card-img-top" alt="...">
+                    <img src="../../public/pin_only.svg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
@@ -52,7 +52,7 @@ export default {
             </div>
             <div class="col-12 col-lg-6">
                 <div class="card m-auto p-3 m-0 w-100">
-                    <img src="../../public/internal/pin_only.svg" class="card-img-top" alt="...">
+                    <img src="../../public/pin_only.svg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
