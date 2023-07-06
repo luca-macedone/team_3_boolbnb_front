c7d14070dbe1f2ed64be9ad380396abeefa07591
RobertoNesta <robertonesta1993@gmail.com>
2023-07-06 16:18:36 +0200

    add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,13 +5,30 @@ export default {
 }
 </script>
 <template>
-  <nav class="navbar">
-    <a class="navbar-brand" href="#">
-      <img height="120" src="../../public/full_logo_horizontal.svg" alt="">
-    </a>
-  </nav>
+  <div class="container">
+    <nav class="navbar">
+      <div class="ft-left d-flex justify-content-between align-items-center">
+        <a class="navbar-brand" href="#">
+          <p>&copy; 2023 Boolbnb, Inc.</p>
+        </a>
+        <ul class="d-flex justify-content-between align-items-center gap-4">
+                <li>Privacy</li>
+                <li>Termini</li>
+                <li>Mappa del sito</li>
+                <li>Dettagli dell'azienda</li>
+                <li>Destinazioni</li>
+            </ul>
+      </div>
+      <div class="ft-right">
+        <ul class="d-flex justify-content-between align-items-center list-unstyled gap-4">
+          <li><i class="fa-solid fa-globe"></i> Italiano (IT)</li>
+          <li>€ EUR</li>
+          <li>Supporto e Risorse ^</li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
-
 <style lang="sass" scoped>
 
 </style>
