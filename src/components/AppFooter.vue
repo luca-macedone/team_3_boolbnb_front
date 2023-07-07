9ef7b1ae686986ecd680ede6f1a83f1318fcc325
luca-macedone <luca.macedone@outlook.it>
2023-07-07 16:11:21 +0200

    minor fixes

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,131 +5,101 @@ export default {
 }
 </script>
 <template>
+  <!-- footerone -->
 
-<!-- footerone -->
+  <footer>
+    <div class="row flex-wrap">
+      <div class="col-12 up_col">
+        <div class="container">
+          <div class="row my-5">
+            <div class="col-3">
+              <h2>BOOL BNB</h2>
 
-<footer>
-      <div class="row flex-wrap">
-        <div class="col-12 up_col">
-          <div class="container">
-            <div class="row my-5">
-              <div class="col-3">
-                <h2>BOOL BNB</h2>
-              
               <ul class="d-flex flex-column gap-3 mt-4">
                 <li><a href="#">Advanced search</a> </li>
                 <li><a href="#">Rent an apartment</a> </li>
                 <li><a href="#">Sponsorship</a> </li>
                 <li><a href="#">Home page</a> </li>
-                
-                
-              </ul>
-              
 
-              </div>  
-              <div class="col-3">
-                <h2>CONTACTS</h2>
-              
+
+              </ul>
+
+
+            </div>
+            <div class="col-3">
+              <h2>CONTACTS</h2>
+
               <ul class="d-flex flex-column gap-3 mt-4">
                 <li><a href="#"> Terms of Use</a></li>
                 <li><a href="#"> Privacy policy</a></li>
                 <li><a href="#"> BoolBnb maps</a></li>
                 <li><a href="#"> Who we are</a></li>
                 <li><a href="#">Contact Us</a> </li>
-                
-              </ul>
-            
 
-              </div> 
-              <div class="col-6">
-                <h2>MADE BY</h2>
-              
+              </ul>
+
+
+            </div>
+            <div class="col-6">
+              <h2>MADE BY</h2>
+
               <ul>
 
-                
-              </ul>
-             
 
-              </div> 
-              
-             
-            
+              </ul>
+
+
             </div>
+
+
+
           </div>
         </div>
-        <div class="col down_col d-flex justify-content-between">
-          
-          <div class="container">
-            <div class="row">
-          
-       
-                <nav class="navbar my-3 d-flex align-items-center">
-                  
-                    <a class="navbar-brand d-flex text-light" href="#">
-                       &copy; 2023 Boolbnb, Inc.
-                    </a>
-                  
-                    <div class="col d-flex justify-content-center py-3 w-100">
-                       <a type="button" class="btn back_btn_card d-flex align-items-center gap-2 shadow w-75 justify-content-center"
-                           href="#">
-                           Sign up Now! 
-                       </a>
-                    </div>
-          
-                    <div class="d-flex align-items-center gap-4">
+      </div>
+      <div class="col down_col d-flex justify-content-between">
 
-                    
-                        <span class="align-self-center"><i class="fa-solid fa-globe"></i> Italiano (IT)</span>
-                        <span class="align-self-center">€ EUR</span>
-                        <span class="align-self-center">Supporto e Risorse</span>
-                      
-                      
-                    <!--   <ul class="d-flex justify-content-between list-unstyled gap-4 h-100 align-items-center">
+        <div class="container">
+          <div class="row">
+
+
+            <nav class="navbar my-3 d-flex align-items-center">
+
+              <a class="navbar-brand d-flex text-light" href="#">
+                &copy; 2023 Boolbnb, Inc.
+              </a>
+
+              <div class="col d-flex justify-content-center py-3 w-100">
+                <a type="button"
+                  class="btn back_btn_card d-flex align-items-center gap-2 shadow w-75 justify-content-center" href="#">
+                  Sign up Now!
+                </a>
+              </div>
+
+              <div class="d-flex align-items-center gap-4">
+
+
+                <span class="align-self-center"><i class="fa-solid fa-globe"></i> Italiano (IT)</span>
+                <span class="align-self-center">€ EUR</span>
+                <span class="align-self-center">Supporto e Risorse</span>
+
+
+                <!--   <ul class="d-flex justify-content-between list-unstyled gap-4 h-100 align-items-center">
                         <li class="vertical_align align-self-center"><i class="fa-solid fa-globe"></i> Italiano (IT)</li>
                         <li class="vertical_align align-self-center">€ EUR</li>
                         <li class="vertical_align align-self-center">Supporto e Risorse</li>
                       </ul> -->
-                    </div>
-                  
-                </nav>
-              
+              </div>
 
-            </div>
+            </nav>
+
+
           </div>
-          
         </div>
 
-  <div class="container">
-    <nav class="navbar">
-      <div class="ft-left d-flex justify-content-between align-items-center">
-        <a class="navbar-brand" href="#">
-          <p>&copy; 2023 Boolbnb, Inc.</p>
-        </a>
-        <ul class="d-flex justify-content-between align-items-center gap-4">
-                <li>Privacy</li>
-                <li>Termini</li>
-                <li>Mappa</li>
-                <li>Dettagli</li>
-                <li>Destinazioni</li>
-            </ul>
       </div>
-      <div class="ft-right">
-        <ul class="d-flex justify-content-between align-items-center list-unstyled gap-4">
-          <li><i class="fa-solid fa-globe"></i> Italiano (IT)</li>
-          <li>€ EUR</li>
-          <li>Supporto e Risorse ^</li>
-        </ul>
-      </div>
-    
+    </div>
   </footer>
-
-
-
-
 </template>
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
 
 
