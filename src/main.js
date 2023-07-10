e73da13ad93bc7a24ce75cd7f0d057bc1c738b01
luca-macedone <luca.macedone@outlook.it>
2023-07-10 16:53:14 +0200

    change order

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { router } from './router';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-import {router} from './router.js'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
