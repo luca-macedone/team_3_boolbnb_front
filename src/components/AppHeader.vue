0cfbc9f0edd4a8ceaa7c58c1dfe817153123ac57
luca-macedone <luca.macedone@outlook.it>
2023-07-11 14:56:02 +0200

    add router link in navbar to home

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,20 @@
 <script>
+import { RouterLink } from 'vue-router';
 export default {
 	name: 'AppHeader',
+	components: ['RouterLink'],
 }
 </script>
 <template>
-
 	<header>
 		<nav class="d-flex justify-content-center align-items-center" id="site_navbar">
 			<a class="dashboard_link d-flex align-items-center gap-2" href="http://127.0.0.1:8000/user/dashboard">
 				<i class="fa-solid fa-circle-user"></i>
 				<span class="">User</span>
 			</a>
-			<div class="logo_wrapper"></div>
+			<RouterLink :to="{ name: 'home' }" class="logo_wrapper"></RouterLink>
 		</nav>
 	</header>
-
 </template>
 
 <style lang="sass" scoped>
