9b0e63562b549aa7fe1fd52a841aeecc35f58efa
luca-macedone <luca.macedone@outlook.it>
2023-07-13 16:12:45 +0200

    remove console logs and fix conditions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,32 +6,32 @@ import Jumbotron from '../components/Jumbotron.vue';
 import { RouterLink } from 'vue-router';
 import { state } from '../state';
 export default {
-	name: "HomeView",
-	components: {
-     RouterLink,
-     Jumbotron,
-     BannerHome,
-     HomeCard,
-     HomeCardBig
-    },
-	data() {
-		return {
-			state,
-		}
-	},
+  name: "HomeView",
+  components: {
+    RouterLink,
+    Jumbotron,
+    BannerHome,
+    HomeCard,
+    HomeCardBig
+  },
+  data() {
+    return {
+      state,
+    }
+  },
   mounted() {
     let sections = document.querySelectorAll('section');
     window.onscroll = () => {
       sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -600;
+        let offset = sec.offsetTop - 600;
         let height = sec.offsetHeight;
 
         if (top >= offset && top < offset + height) {
           sec.classList.add('show_animate');
-        } 
-        
-        else{
+        }
+
+        else {
           sec.classList.remove('show_animate');
         }
 
@@ -43,57 +43,51 @@ export default {
 </script>
 
 <template>
+  <Jumbotron></Jumbotron>
+
+  <BannerHome></BannerHome>
 
 
 
-  <Jumbotron></Jumbotron> 
-  
-    <BannerHome></BannerHome>
-  
-  
-
-	<div class="container ">
-		<div class="row justify-content-center">
+  <div class="container ">
+    <div class="row justify-content-center" v-if="state.apartments && state.apartments.length > 0">
       <section id="section_home" class="sec_2  flex-column">
         <div class="d-flex justify-content-center animate  my-2 flex-column animate">
           <div class="d-flex justify-content-center flex-wrap">
-            <img src="/logo_horizontal.svg" class=" img-fluid" alt="bool bnb logo"> 
-                <img src="/most_wanted_logo.svg" class="most_wanted img-fluid" alt="most wanted apartments">
-          </div>  
-         
-            <hr class="">
-            
-          
+            <img src="/logo_horizontal.svg" class=" img-fluid" alt="bool bnb logo">
+            <img src="/most_wanted_logo.svg" class="most_wanted img-fluid" alt="most wanted apartments">
+          </div>
+
+          <hr class="">
+
+
 
         </div>
-      
-			
 
-			<!-- card display ---------------------->
-      
+
+
+        <!-- card display ---------------------->
+
         <div class="row g-3 mt-3 mb-5 animate" v-if="state.apartments">
 
-        <HomeCardBig v-for="(apartment, index) in state.apartments.slice(0, 1)" :apartment="apartment" :index="index">
-        </HomeCardBig>
+          <HomeCardBig v-for="(apartment, index) in state.apartments.slice(0, 1)" :apartment="apartment" :index="index">
+          </HomeCardBig>
 
-        <HomeCard  v-for="(apartment, index) in state.apartments.slice(1, 7)" :apartment="apartment" :index="index">
-        </HomeCard>
+          <HomeCard v-for="(apartment, index) in state.apartments.slice(1, 7)" :apartment="apartment" :index="index">
+          </HomeCard>
 
         </div>
 
       </section>
 
-      
+
 
     </div>
 
 
-	</div>
-	
+  </div>
 </template>
 
 <style lang="scss">
 @use '../styles/app.scss';
-
-
 </style>
