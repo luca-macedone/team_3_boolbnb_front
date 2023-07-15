9e2c3b864a449e4fd83ebc58ebb7a3791466bcda
luca-macedone <luca.macedone@outlook.it>
2023-07-15 16:31:36 +0200

    optimize image path

--- src/components/ApartmentCard.vue
+++ src/components/ApartmentCard.vue
@@ -17,7 +17,7 @@ export default {
     <div class="col-12 col-md-6 col-lg-4 text-decoration-none">
         <RouterLink :to="{ name: 'apartment', params: { slug: slug } }" class="h-100">
             <div class="dashboard_card apartment_card shadow p-4">
-                <img :src="state.getImageFromPath(image)" onerror="this.onerror=null; this.src='/public/missing_img_v2.svg'"
+                <img :src="state.getImageFromPath(image)" onerror="this.onerror=null; this.src='/missing_img_v2.svg'"
                     class="card-img" :alt="title + ' img'">
                 <div class="card_img_overlay">
                     <div class="apartment_info d-flex flex-column align-items-center justify-content-between">
