1c9593e203648cec9ab74a3aa5f694c21dd75cd8
fabiodelli <ntqdel@hotmail.com>
2023-07-16 12:12:20 +0200

    add fixed to nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,7 @@ export default {
 }
 </script>
 <template>
-	<header class="mb-5 shadow">
+	<header class="mb-5 shadow position-fixed w-100 z_index">
 		<nav class="d-flex justify-content-center align-items-center" id="site_navbar">
 			<a class="dashboard_link d-flex align-items-center gap-2" href="http://127.0.0.1:8000/user/dashboard">
 				<i class="fa-solid fa-circle-user"></i>
