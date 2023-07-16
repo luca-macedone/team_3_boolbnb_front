8e1b8680de87f3e0f13d2e91541194cd7233a8e6
luca-macedone <luca.macedone@outlook.it>
2023-07-16 11:34:30 +0200

    remove d-flex on row 44

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,70 +6,71 @@ import Jumbotron from '../components/Jumbotron.vue';
 import { RouterLink } from 'vue-router';
 import { state } from '../state';
 export default {
-  name: "HomeView",
-  components: {
-    RouterLink,
-    Jumbotron,
-    BannerHome,
-    HomeCard,
-    HomeCardBig
-  },
-  data() {
-    return {
-      state,
-    }
-  },
-  mounted() {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        //console.log(entry)
-        if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-        } else {
-          entry.target.classList.remove('show');
-        }
-      });
-    })
-    const hidden_elements = document.querySelectorAll('.hidden');
-    hidden_elements.forEach((el) => observer.observe(el));
-  },
+	name: "HomeView",
+	components: {
+		RouterLink,
+		Jumbotron,
+		BannerHome,
+		HomeCard,
+		HomeCardBig
+	},
+	data() {
+		return {
+			state,
+		}
+	},
+	mounted() {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				//console.log(entry)
+				if (entry.isIntersecting) {
+					entry.target.classList.add('show');
+				} else {
+					entry.target.classList.remove('show');
+				}
+			});
+		})
+		const hidden_elements = document.querySelectorAll('.hidden');
+		hidden_elements.forEach((el) => observer.observe(el));
+	},
 }
 </script>
 
 <template>
-  <div class="container">
-    <Jumbotron />
-  </div>
+	<div class="container">
+		<Jumbotron />
+	</div>
 
-  <div class="container d-flex justify-content-center">
-    <div class="row justify-content-center w-100" v-if="state.apartments && state.apartments.length > 0">
-      <section id="section_home" class="sec_2 hidden d-flex flex-column w-100 align-items-center">
-        <div class="d-flex justify-content-center  my-2 flex-column ">
-          <div class="d-flex justify-content-center flex-wrap ">
-            <img src="/logo_horizontal.svg" class=" img-fluid" alt="bool bnb logo">
-            <img src="/most_wanted_logo.svg" class="most_wanted img-fluid" alt="most wanted apartments">
-          </div>
-          <hr class="">
-        </div>
-
-
-
-        <!-- card display ---------------------->
-
-        <div class="row g-3 mt-3 mb-5 w-100 " v-if="state.apartments">
-
-          <HomeCard v-for="(apartment, index) in state.apartments.slice(0, 6)" :apartment="apartment" :index="index" />
-
-        </div>
-
-      </section>
-
-    </div>
-
-    <BannerHome />
+	<div class="container justify-content-center">
+		<div class="row justify-content-center w-100" v-if="state.apartments && state.apartments.length > 0">
+			<section id="section_home" class="sec_2 hidden d-flex flex-column w-100 align-items-center">
+				<div class="d-flex justify-content-center  my-2 flex-column ">
+					<div class="d-flex justify-content-center flex-wrap ">
+						<img src="/logo_horizontal.svg" class="img-fluid" alt="bool bnb logo">
+						<img src="/most_wanted_logo.svg" class="most_wanted img-fluid" alt="most wanted apartments">
+					</div>
+					<hr class="">
+				</div>
 
 
-  </div>
+
+				<!-- card display ---------------------->
+
+				<div class="row g-3 mt-3 mb-5 w-100 " v-if="state.apartments">
+
+					<HomeCard v-for="(apartment, index) in state.apartments.slice(0, 6)" :apartment="apartment"
+						:index="index" />
+
+				</div>
+
+			</section>
+
+		</div>
+
+		<BannerHome />
+
+
+	</div>
 </template>
 
 <style lang="scss">
